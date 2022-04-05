5a11e30b9f314e07bce1082906ff4cde263158b4
paulleerentveld <paul@veldnet.com.au>
2022-04-06 09:56:21 +1000

    add static methods for lab

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  static capitalize(value) {
+    return value.charAt(0).toUpperCase() + value.slice(1);
+
+  }
+  static sanitize(value) {
+    return value.replace(/[^A-Za-z0-9-' ]+/g, '');
+
+
+  }
+  static titleize(value) {
+    let arr = [];
+    let exclude = ['the','a','an','but','of','and','for','at','by','from']
+    arr = value.split(' ');
+    return arr.map(word => {
+      return exclude.includes(word) && word !== arr[0] ? [word] : word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  }
 }
